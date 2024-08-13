802586c69ca31e274a34e3dfffc5d453453f01ff
Abhinav_admin <abhinav_admin@BC-MAC-M003.local>
2024-08-13 10:31:49 +0530

    Parent list added

--- create.py
+++ create.py
@@ -3,6 +3,8 @@ from py2neo import Graph
 from dotenv import load_dotenv
 import os
 
+# Load environment variables
+load_dotenv()
 neo4j_password = os.getenv('NEO4J_PASSWORD')
 
 # Load YAML data from file
@@ -15,23 +17,23 @@ graph = Graph("bolt://localhost:7687", auth=("neo4j", neo4j_password))
 # Clear existing data (optional, be cautious with this)
 graph.run("MATCH (n) DETACH DELETE n")
 
-# Create nodes and relationships based on the YAML data
+# Create nodes and relationships based on the YAML file
 for node, properties in data.items():
     name = properties['Name']
     desc = properties['Desc']
-    parent = properties['parent']
-    
+    parents = properties['parent']
+
     # Create the node
     query = f"CREATE (n:{name} {{Name: '{name}', Desc: '{desc}'}})"
     graph.run(query)
-    
-    # Create relationships with parent if parent exists
-    if parent != 'None':
-        parent_query = f"MATCH (p:{parent}), (c:{name}) CREATE (p)-[:PARENT_OF]->(c)"
-        graph.run(parent_query)  
-        child_query = f"MATCH (p:{parent}), (c:{name}) CREATE (c)-[:CHILD_OF]->(p)"
-        graph.run(child_query)  
 
-
+    # Handle parent relationships
+    if parents and parents != []:
+        for parent in parents:
+            # Create relationships with parent
+            parent_query = f"MATCH (p:{parent}), (c:{name}) CREATE (p)-[:PARENT_OF]->(c)"
+            graph.run(parent_query)
+            child_query = f"MATCH (p:{parent}), (c:{name}) CREATE (c)-[:CHILD_OF]->(p)"
+            graph.run(child_query)
 
 print("Data imported successfully.")
